30a274deaf54d33eceda350f77903c6d737ecf70
Ivy Maundu <ivymaundu03@gmail.com>
2024-09-08 15:29:21 +0300

    home and login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {  BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx'
-import './index.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      {/* <Router> */}
+      <App />
+      {/* </Router> */}
+  </React.StrictMode>,
 )
---
+++ src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import Layout from '../components/Layout';
+import Form from '../components/Form';
+// import {handleFormSubmit} from '../Components/Form'
+import '../style/Login.css'
+
+interface FormValues{
+    email:string;
+    password:string;
+}
+
+export default function Login(){
+    function onsubmit(data: FormValues) {
+        // e.preventDefault()
+        console.log("lazy", data)
+    }
+    return(
+        <div className="login-container">
+            <Layout/>
+            <div className="login-inner-container">
+            <Form
+            className='login-form'
+             fields={[
+                { name: 'email', type: 'email', placeholder: 'Enter Email ...', required: true },
+                { name: 'password', type: 'password', placeholder: 'Password', required: true },
+              ]} 
+              onSubmit={onsubmit}
+            /> 
+            </div>
+        </div>
+    )
+
+}
